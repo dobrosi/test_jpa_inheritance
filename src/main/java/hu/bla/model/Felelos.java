1c909b03bd65f3734527281eaaf0b8ead6e2b0ee
andris <andris@andris-dell>
2016-07-10 10:22:08 +0200

    ehcache

--- src/main/java/hu/bla/model/Felelos.java
+++ src/main/java/hu/bla/model/Felelos.java
@@ -1,8 +1,8 @@
 package hu.bla.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,13 +10,20 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "DTYPE")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public abstract class Felelos {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@OneToOne(cascade={CascadeType.PERSIST})
+	private Address address;
 
 	private String name;
 
@@ -25,6 +32,9 @@ public abstract class Felelos {
 
 	public Felelos(String name) {
 		this.name = name;
+		Address address = new Address();
+		address.setStreet("Sarfu");
+		setAddress(address);
 	}
 
 	public String getName() {
@@ -34,12 +44,20 @@ public abstract class Felelos {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getReferenceName() {
 		return "felelos";
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
 	}
 }
