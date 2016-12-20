9d38f6e59eae4d82f6cc925535ab37cb03e7d443
dobrosi <dobrosi.andras@hmei.hu>
2016-12-20 15:29:54 +0100

    Truncated history

---
+++ src/main/java/hu/bla/model/IratBase.java
@@ -0,0 +1,38 @@
+package hu.bla.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class IratBase {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+	private Felelos felelos;
+	
+	public IratBase() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Felelos getFelelos() {
+		return felelos;
+	}
+
+	public void setFelelos(Felelos felelos) {
+		this.felelos = felelos;
+	}
+}
